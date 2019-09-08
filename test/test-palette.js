468f305eca293795d45216ed13a760aa16b62e4d
Kevin Scroggins <nitro404@gmail.com>
2019-09-08 17:53:05 -0400

    WIP Additional Palette unit tests and cleanup

--- test/test-palette.js
+++ test/test-palette.js
@@ -120,7 +120,7 @@ describe("Duke3D", function() {
 			it("should allow a valid palette subclass to be instantiated if all abstract features are implemented", function() {
 				let paletteTest = null;
 
-				expect(function() { paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Test.BIN"); }).to.not.throw();
+				expect(function() { paletteTest = new PaletteTest("", paletteTestFileType, "Test.BIN"); }).to.not.throw();
 				expect(paletteTest.paletteSubclass).to.equal(PaletteTest);
 				expect(paletteTest.data.equals(Buffer.from(""))).to.equal(true);
 				expect(paletteTest.fileType).to.equal(paletteTestFileType);
@@ -144,13 +144,13 @@ describe("Duke3D", function() {
 
 			describe("fileTypes", function() {
 				it("should be an empty array by default", function() {
-					const testPalette = new PaletteTest(Buffer.from(""), paletteTestFileType, "default.BIN");
+					const testPalette = new PaletteTest("", paletteTestFileType, "default.BIN");
 
 					expect(testPalette.fileTypes).to.be.an("array").that.is.empty;
 				});
 
 				it("should be read-only", function() {
-					const testPalette = new PaletteTest(Buffer.from(""), paletteTestFileType, "default.BIN");
+					const testPalette = new PaletteTest("", paletteTestFileType, "default.BIN");
 
 					expect(function() { testPalette.fileTypes = []; }).to.throw(TypeError);
 				});
@@ -158,7 +158,7 @@ describe("Duke3D", function() {
 
 			describe("fileType", function() {
 				it("should allow valid file types to be assigned", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Data.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Data.BIN");
 					const testFileType = new Palette.FileType("Test", "TEST");
 
 					expect(paletteTest.fileType).to.equal(paletteTestFileType);
@@ -169,7 +169,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should assign the special invalid file type when invalid file types are assigned", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Data.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Data.BIN");
 					const invalidFileType = new Palette.FileType(-32, "Test", "TEST");
 
 					paletteTest.fileType = invalidFileType;
@@ -181,7 +181,7 @@ describe("Duke3D", function() {
 
 			describe("filePath", function() {
 				it("should allow valid strings to be assigned", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Example.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Example.BIN");
 
 					paletteTest.filePath = "Valid.BIN";
 
@@ -189,7 +189,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should trim string values assigned to it", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Example.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Example.BIN");
 
 					paletteTest.filePath = "  data/EXAMPLE.BIN\t";
 
@@ -197,7 +197,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should assign a value of null when invalid values are provided", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Telepathy.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Telepathy.BIN");
 
 					paletteTest.filePath = { };
 
@@ -225,7 +225,7 @@ describe("Duke3D", function() {
 						static getFileTypeForData() { }
 					}
 
-					const paletteTest = new PaletteTestFilePathChanged(Buffer.from(""), paletteTestFileType, "Test.BIN");
+					const paletteTest = new PaletteTestFilePathChanged("", paletteTestFileType, "Test.BIN");
 
 					sinon.spy(paletteTest, "onFilePathChanged");
 
@@ -240,7 +240,7 @@ describe("Duke3D", function() {
 
 			describe("data", function() {
 				it("should accept ByteBuffer values", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Testing.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Testing.BIN");
 					const byteBuffer = new ByteBuffer("With No Head");
 
 					paletteTest.data = byteBuffer;
@@ -249,7 +249,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should accept Buffer values", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Testing.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Testing.BIN");
 					const buffer = Buffer.from("The Edge of Sanity");
 
 					paletteTest.data = buffer;
@@ -258,7 +258,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should accept array values", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Testing.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Testing.BIN");
 
 					paletteTest.data = [65, 66, 67];
 
@@ -266,7 +266,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should accept string values", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Testing.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Testing.BIN");
 
 					paletteTest.data = "Seizure";
 
@@ -274,7 +274,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should invoke createNewData if no data or invalid data is provided", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Testing.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Testing.BIN");
 
 					sinon.spy(paletteTest, "createNewData");
 
@@ -287,7 +287,7 @@ describe("Duke3D", function() {
 				});
 
 				it("should invoke validateData if there is any data assigned to the object", function() {
-					const paletteTest = new PaletteTest(Buffer.from(""), paletteTestFileType, "Test.BIN");
+					const paletteTest = new PaletteTest("", paletteTestFileType, "Test.BIN");
 
 					sinon.spy(paletteTest, "validateData");
 
@@ -316,7 +316,7 @@ describe("Duke3D", function() {
 						static getFileTypeForData() { }
 					}
 
-					const paletteTest = new PaletteTestDataChanged(Buffer.from(""), paletteTestFileType, "Test.BIN");
+					const paletteTest = new PaletteTestDataChanged("", paletteTestFileType, "Test.BIN");
 
 					sinon.spy(paletteTest, "onDataChanged");
 
@@ -364,7 +364,29 @@ describe("Duke3D", function() {
 				expect(Palette.isExtendedBy).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should return true for a valid class that extends Palette", function() {
+				expect(Palette.isExtendedBy(PaletteTest)).to.equal(true);
+			});
+
+			it("should return false for a valid class that does not extend Palette", function() {
+				class TestClass { }
+
+				expect(Palette.isExtendedBy(TestClass)).to.equal(false);
+			});
+
+			it("should return true for an object instance of a valid class that extends Palette", function() {
+				const testPalette = new PaletteTest();
+
+				expect(Palette.isExtendedBy(testPalette)).to.equal(true);
+			});
+
+			it("should return false for Palette", function() {
+				expect(Palette.isExtendedBy(Palette)).to.equal(false);
+			});
+
+			it("should return false for non-object values", function() {
+				expect(Palette.isExtendedBy(null)).to.equal(false);
+			});
 		});
 
 		describe("static numberOfPaletteTypes", function() {
@@ -428,7 +450,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.createNewData).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.createNewData(); }).to.throw();
+			});
 		});
 
 		describe("getFileName", function() {
@@ -436,15 +460,55 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.getFileName).to.be.an.instanceof(Function);
 			});
 
-			// TODO
-		});
+			it("should return null if the file path is empty or null", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "");
 
-		describe("getExtension", function() {
-			it("should be a function", function() {
-				expect(Palette.prototype.getExtension).to.be.an.instanceof(Function);
+				expect(testPalette.getFileName()).to.equal(null);
+
+				testPalette.filePath = null;
+
+				expect(testPalette.getFileName()).to.equal(null);
 			});
 
-			// TODO
+			it("should extract the file name from a valid file path", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "VALID.BIN");
+
+				expect(testPalette.getFileName()).to.equal("VALID.BIN");
+
+				testPalette.filePath = "some/path/to/FILE.BIN";
+
+				expect(testPalette.getFileName()).to.equal("FILE.BIN");
+			});
+		});
+
+		describe("getFileExtension", function() {
+			it("should be a function", function() {
+				expect(Palette.prototype.getFileExtension).to.be.an.instanceof(Function);
+			});
+
+			it("should return an empty string if the file path is empty", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "");
+
+				expect(testPalette.getFileExtension()).to.be.a("string").that.is.empty;
+			});
+
+			it("should return bull if the file path is null", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "");
+
+				testPalette.filePath = null;
+
+				expect(testPalette.getFileExtension()).to.equal(null);
+			});
+
+			it("should extract the file extension from a valid file path", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "VALID.BIN");
+
+				expect(testPalette.getFileExtension()).to.equal("BIN");
+
+				testPalette.filePath = "some/path/to/FILE.EXE";
+
+				expect(testPalette.getFileExtension()).to.equal("EXE");
+			});
 		});
 
 		describe("numberOfFileTypes", function() {
@@ -516,7 +580,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.getPaletteDescription).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.getPaletteDescription(); }).to.throw();
+			});
 		});
 
 		describe("getPaletteDescriptions", function() {
@@ -540,7 +606,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.getPixel).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.getPixel(); }).to.throw();
+			});
 		});
 
 		describe("lookupPixel", function() {
@@ -548,7 +616,34 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.lookupPixel).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should return null for invalid lookup values", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				expect(testPalette.lookupPixel(-Infinity, 0)).to.equal(null);
+			});
+
+			it("should return null for lookup values that are negative or exceed the palette size", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				expect(testPalette.lookupPixel(-1, 0)).to.equal(null);
+				expect(testPalette.lookupPixel(Palette.NumberOfColours, 0)).to.equal(null);
+			});
+
+			it("should invoke getPixel with the correct x / y co-ordinates and index values", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				sinon.stub(testPalette, "getPixel").callsFake(function(x, y, index) {
+					return null;
+				});
+
+				for(let i = 0; i < 256; i++) {
+					expect(function() { testPalette.lookupPixel(i, i + 1000); }).to.not.throw();
+					expect(testPalette.getPixel.callCount).to.equal(i + 1);
+					expect(testPalette.getPixel.getCall(i).calledWithExactly(i % 16, Math.floor(i / 16), i + 1000)).to.equal(true);
+				}
+
+				testPalette.getPixel.restore();
+			});
 		});
 
 		describe("updatePixel", function() {
@@ -556,7 +651,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.updatePixel).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.updatePixel(); }).to.throw();
+			});
 		});
 
 		describe("getColourData", function() {
@@ -564,7 +661,43 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.getColourData).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should return null for invalid indexes", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				expect(testPalette.getColourData(Infinity)).to.equal(null);
+			});
+
+			it("should return null for indexes that are negative or exceed the number of sub palettes", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				expect(testPalette.getColourData(-1)).to.equal(null);
+				expect(testPalette.getColourData(1)).to.equal(null);
+			});
+
+			it("should correctly obtain all of the pixels in a palette as an array of colour objects", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				sinon.stub(testPalette, "getPixel").callsFake(function(x, y, index) {
+					return new Colour(x, y, index);
+				});
+
+				const colourData = testPalette.getColourData(0);
+
+				expect(colourData).to.be.an("array").that.is.not.empty;
+				expect(colourData.length).to.equal(256);
+
+				for(let i = 0; i < colourData.length; i++) {
+					expect(colourData[i]).to.be.an.instanceof(Colour);
+				}
+
+				expect(testPalette.getPixel.callCount).to.equal(256);
+
+				for(let i = 0; i < testPalette.getPixel.callCount; i++) {
+					expect(testPalette.getPixel.getCall(i).calledWithExactly(i % 16, Math.floor(i / 16), 0)).to.equal(true);
+				}
+
+				testPalette.getPixel.restore();
+			});
 		});
 
 		describe("getAllColourData", function() {
@@ -572,7 +705,39 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.getAllColourData).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should correctly obtain all of the pixels for multiple sub-palettes as an array of colour objects", function() {
+				const testPalette = new PaletteTest("", paletteTestFileType, "DATA.BIN");
+
+				sinon.stub(testPalette, "numberOfSubPalettes").callsFake(function() {
+					return 2;
+				});
+
+				sinon.stub(testPalette, "getPixel").callsFake(function(x, y, index) {
+					return new Colour(x, y, index);
+				});
+
+				const colourData = testPalette.getAllColourData();
+
+				expect(colourData).to.be.an("array").that.is.not.empty;
+				expect(colourData.length).to.equal(512);
+
+				for(let i = 0; i < colourData.length; i++) {
+					expect(colourData[i]).to.be.an.instanceof(Colour);
+				}
+
+				expect(testPalette.getPixel.callCount).to.equal(512);
+
+				let c = 0;
+
+				for(let i = 0; i < 2; i++) {
+					for(let j = 0; j < 256; j++) {
+						expect(testPalette.getPixel.getCall(c++).calledWithExactly(j % 16, Math.floor(j / 16), i)).to.equal(true);
+					}
+				}
+
+				testPalette.getPixel.restore();
+				testPalette.numberOfSubPalettes.restore();
+			});
 		});
 
 		describe("updateColourData", function() {
@@ -580,7 +745,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.updateColourData).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.updateColourData(); }).to.throw();
+			});
 		});
 
 		describe("updateAllColourData", function() {
@@ -596,7 +763,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.fillWithColour).to.be.an.instanceof(Function);
 			});
 
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.fillWithColour(); }).to.throw();
+			});
 		});
 
 		describe("fillAllWithColour", function() {
@@ -668,8 +837,9 @@ describe("Duke3D", function() {
 				expect(Palette.prototype.validateData).to.be.an.instanceof(Function);
 			});
 
-			
-			// TODO
+			it("should be abstract and throw an error if invoked directly", function() {
+				expect(function() { Palette.prototype.validateData(); }).to.throw();
+			});
 		});
 
 		describe("isValid", function() {
